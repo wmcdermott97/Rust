a942407be80c5e2e39c6f12211b290158a9626ce
wmcdermott-home <wmcdermott.math@gmail.com>
2024-03-18 15:57:28 -0400

    Errors fixed on basic KW, compiled

--- BasicKWs.rs
+++ BasicKWs.rs
@@ -1,31 +1,35 @@
 fn main() {
-  println!(gcd(14, 10));
-  println!(euler_totient(10));
+  println!("{}", gcd(14, 10));
+  println!("{}", euler_totient(10));
 }
 
 // Demonstrate the if keyword
-fn abs(x: i32) -> i32 {
+fn abs(mut x: i32) -> i32 {
   if x < 0 {
-    x = -x
+    x = -x;
   }
-  return x
+  return x;
 }
 
 // Demonstrate the while keyword
-fn gcd(a: i32, b: i32) -> i32 {
-  while b !== 0 {
-    s = a % b
-    a = b
-    b = s
+fn gcd(mut a: i32, mut b: i32) -> i32 {
+  a = abs(a);
+  b = abs(b);
+  while b != 0 {
+    let s = a % b;
+    a = b;
+    b = s;
   }
+  return a;
 }
 
 // Demonstrate the for keyword
 fn euler_totient(n: i32) -> i32 {
-  let sum = 0
+  let mut sum = 0;
   for i in 1..(n - 1) {
     if gcd(i, n) == 1 {
-      sum += 1
+      sum += 1;
     }
   }
+  return sum;
 }
