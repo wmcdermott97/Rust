63ab39a378e1419c13cb362717b0c250cc48cb31
wmcdermott-home <wmcdermott.math@gmail.com>
2024-03-21 01:24:49 -0400

    One last change to make it de-duplicate

--- random/src/main.rs
+++ random/src/main.rs
@@ -1,14 +1,15 @@
 
 fn main() {
-  println!("{}", compute_irrational(3));
+  println!("{}", compute_irrational(100));
 }
 fn compute_irrational(n: u32) -> String {
-  let mut num: String = "0.0".to_string();
+  let mut num: String = "0.00".to_string();
   let mut ones_pos: usize;
-  for _i in 0..n {
+  for i in 0..n {
     ones_pos = num.find('.').unwrap() - 1;
     num = string_add_at(num, ones_pos, 1);
-    num = string_simplify(num);
+    num = string_flatten(num);
+    println!("{} gives {}", i + 1, num);
   }
   return num;
 }
@@ -17,7 +18,7 @@ fn string_add_at(num: String, pos: usize, add: i8) -> String {
   let mut num_cpy: String = num.clone();
   let (first, rest) = num_cpy.split_at_mut(pos);
   let (ones, second) = rest.split_at_mut(1);
-  println!("in string_add_at: {}, {}, {}", first, ones, second); // nice debug
+  // println!("in string_add_at: {}, {}, {}", first, ones, second); // nice debug
   let ones_next: String = char_add(ones, add);
   return first.to_owned() + &ones_next + second;
 }
@@ -33,17 +34,16 @@ fn char_add(str: &str, add: i8) -> String {
   }
 }
 
-fn string_verify(num: String) -> bool {
+fn string_verify_flat(num: String) -> bool {
   // println!("{}", '.' as u8); outputs 46, so '.' < '3' is true
   return num.chars().all(|x: char| x < '3');
-  // need to deal with adjacent twos still....
 }
 
-fn string_simplify(num: String) -> String {
-  if string_verify(num.clone()) {
+fn string_flatten(num: String) -> String {
+  if string_verify_flat(num.clone()) {
     return num;
   }
-  println!("In string_simplify beginning: {}", num);
+  //println!("In string_flatten beginning: {}", num);
   // remove decimal
   let mut dec_pos: usize = num.find('.').unwrap();
   let mut num_spl = num.clone();
@@ -54,7 +54,7 @@ fn string_simplify(num: String) -> String {
   // since we failed to verify, some character is greater than '2' - make everything smaller
   let mut pos: usize = new_num.find(|c: char| c > '2').unwrap();
   // deal with cases where we need to add zeroes near the ends
-  if pos >= num.len() - 2 {
+  if pos >= new_num.len() - 2 {
     new_num = new_num + "00";
   }
   if pos == 0 {
@@ -66,8 +66,14 @@ fn string_simplify(num: String) -> String {
   new_num = string_add_at(new_num, pos - 1, 1);
   new_num = string_add_at(new_num, pos, -3);
   new_num = string_add_at(new_num, pos + 2, 2);
-  println!("{}", new_num);
+  //println!("In string_flatten ending: {}", new_num);
   // put the decimal back in its place
-  let (new1, new2) = new_num.split_at_mut(dec_pos - 1);
-  return string_simplify(new1.to_string() + "." + &new2);
+  let (new1, new2) = new_num.split_at_mut(dec_pos);
+  return string_flatten(new1.to_string() + "." + &new2);
+}
+
+fn string_complete(num: String) -> String {
+  let mut new_num: String = num.clone();
+
+  return num;
 }
